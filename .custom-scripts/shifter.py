4fdb71aea7985f36d9c61e6db298742a5d759f81
tnoff <tylernorth18@gmail.com>
2016-08-21 15:14:50 -0700

    add some more randomness to desktop shifter

--- .custom-scripts/shifter.py
+++ .custom-scripts/shifter.py
@@ -7,11 +7,15 @@ import time
 def main():
     home_directory = os.path.expanduser('~')
     picture_directory = os.path.join(home_directory, 'Pictures', 'Wallpapers')
-
     sleep_seconds = 300
 
+    files = os.listdir(picture_directory)
     while True:
-        file_name = os.path.join(picture_directory, random.choice(os.listdir(picture_directory)))
+        if len(files) == 0:
+            files = os.listdir(picture_directory)
+        for _ in range(100):
+            random.shuffle(files)
+        file_name = os.path.join(picture_directory, files.pop(0))
         os.system('gsettings set org.gnome.desktop.background picture-uri "file:///%s"' % file_name)
         time.sleep(sleep_seconds)
 
