db712b92d38285a8da32ef8b3a7a8bf1dd6e5533
tnoff <tylernorth18@gmail.com>
2016-09-10 16:37:50 -0700

    fix up picture backups script

--- .custom-scripts/backup-pictures.sh
+++ .custom-scripts/backup-pictures.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
+# input: backup-pictures.sh <picture-dir> <picture-backup-dir>
+
+# allow command line input, not neeeded
 
 picture_dir="$HOME/Pictures"
 backup_picture_dir="$HOME/Pictures-Backups"
 
-rm -rf ${backup_picture_dir}
+if [ -z "${1}" ]; then
+    echo "Picture dir not given, ${picture_dir} will be used"
+else
+    picture_dir="${1}"
+fi
 
-echo "Making backup dirs $backup_picture_dir"
+if [ -z "${2}" ]; then
+    echo "Backup dir not given, ${backup_picture_dir} will be used"
+else
+    backup_picture_dir="${2}"
+fi
+
+echo "Removing any files in backup dir"
+rm -rf "${backup_picture_dir}"
+
+echo "Making backup dirs ${backup_picture_dir}"
 find "${picture_dir}" -type d -print0 | while IFS='' read -r -d '' directory;
 do
     backup_dir=${directory#$picture_dir}
     backup_dir_full=${backup_picture_dir}${backup_dir}
     echo "Making dir ${backup_dir_full}"
-    mkdir -p $backup_dir_full
+    mkdir -p "${backup_dir_full}"
 done
 
 echo "Starting picture stuff $picture_dir"
@@ -20,7 +36,7 @@ do
     relative=$backup_picture_dir${picture#$picture_dir}
     backup_name=$(echo "${relative}" | sed -r 's/.jpg$/.lep/')
     echo "Making backup of file ${picture}"
-    lepton "$picture" "$backup_name" > /dev/null 2>&1
+    lepton "${picture}" "${backup_name}" > /dev/null 2>&1
 done
 
 echo "Starting non picture stuff $picture_dir"
