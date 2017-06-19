1c63d3e7aff3cb1812840912f905659932c5fd0a
tnoff <tylernorth18@gmail.com>
2017-06-18 20:21:04 -0700

    more fixes to backup

--- .custom-scripts/backup-all-the-things.sh
+++ .custom-scripts/backup-all-the-things.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euf -o pipefail
+
 LOCAL_ROOT="${HOME}"
 MUSIC_EXT="/Music/"
 VIDEO_EXT="/Videos/"
@@ -14,31 +16,35 @@ fi
 
 destination=`echo "$1" | sed -r 's/\/$//'`
 
-echo "Going to run the following rysnc commands, that cool?"
-echo "rsync -rvuc --copy-links ${LOCAL_ROOT}${DOCS_EXT} ${destination}${DOCS_EXT} --delete"
-echo "rsync -rvuc ${LOCAL_ROOT}${MUSIC_EXT} ${destination}${MUSIC_EXT} --delete"
-echo "rsync -rvuc ${LOCAL_ROOT}${VIDEO_EXT} ${destination}${VIDEO_EXT} --delete"
-echo "rsync -rvuc ${LOCAL_ROOT}${PICS_EXT} ${destination}${PICS_EXT} --delete"
+echo "Will run the following rysnc commands"
+docs_sync="rsync -rvputogcL ${LOCAL_ROOT}${DOCS_EXT} ${destination}${DOCS_EXT} --delete"
+music_sync="rsync -rvuptog ${LOCAL_ROOT}${MUSIC_EXT} ${destination}${MUSIC_EXT} --delete"
+pic_sync="rsync -rvuptog ${LOCAL_ROOT}${PICS_EXT} ${destination}${PICS_EXT} --delete"
+video_sync="rsync -rvuptog ${LOCAL_ROOT}${VIDEO_EXT} ${destination}${VIDEO_EXT} --delete"
+echo "${docs_sync}"
+echo "${music_sync}"
+echo "${pic_sync}"
+echo "${video_sync}"
 
-echo "Enter: y/n"
+echo "Enter (Y|y) to continue"
 read response
 
 if [ "${response}" == "y" ] || [ "${response}" == "Y" ]; then
-    echo "Running sync on docs"
-    rsync -rvuc --copy-links ${LOCAL_ROOT}${DOCS_EXT} ${destination}${DOCS_EXT} --delete
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "Running sync on music"
-    rsync -rvuc ${LOCAL_ROOT}${MUSIC_EXT} ${destination}${MUSIC_EXT} --delete
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "Running sync on videos"
-    rsync -rvuc ${LOCAL_ROOT}${VIDEO_EXT} ${destination}${VIDEO_EXT} --delete
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "Running sync on pictures"
-    rsync -rvuc ${LOCAL_ROOT}${PICS_EXT} ${destination}${PICS_EXT} --delete
+    echo "Running doc sync"
+    eval "${docs_sync}"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "Running music sync"
+    eval "${music_sync}"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "Running pic sync"
+    eval "${pic_sync}"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "Running video sync"
+    eval "${video_sync}"
 fi
