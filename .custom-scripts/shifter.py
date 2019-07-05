c767e771594b432d62a27065854b35b7c00af917
tnoff <1326564+tnoff@users.noreply.github.com>
2019-07-04 18:13:30 -0700

    Rework shifter file, run in crontab instead

--- .custom-scripts/shifter.py
+++ .custom-scripts/shifter.py
@@ -3,10 +3,10 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 import random
-import time
 
 HOME_DIRECTORY = os.path.expanduser('~')
 PICTURE_DIRECTORY = os.path.join(HOME_DIRECTORY, 'Pictures', 'Wallpapers')
+PICTURE_LIST_FILE = os.path.join(HOME_DIRECTORY, '.shifter_files')
 LOG_FILE = os.path.join(HOME_DIRECTORY, '.shifter_log')
 WAIT_INTERVAL = 300
 
@@ -24,28 +24,37 @@ def setup_logger():
     return logger
 
 def reset_files():
-    files = os.listdir(PICTURE_DIRECTORY)
-    for _ in range(len(files) * 2):
-        random.shuffle(files)
-    return files
+    # First attempt to read files
+    try:
+        with open(PICTURE_LIST_FILE, 'r') as reader:
+            files = reader.read().split('\n')
+    except FileNotFoundError:
+        files = []
+    # If no files left, reset picture file
+    if len(files) == 0:
+        files = os.listdir(PICTURE_DIRECTORY)
+        # Shuffle a couple of times before writing
+        for _ in range(len(files) * 2):
+            random.shuffle(files)
+
+    # Make sure file exists when getting it
+    while True:
+        # Grab first file to return later
+        file_name = os.path.join(PICTURE_DIRECTORY, files.pop(0))
+        if os.path.isfile(file_name):
+            break
+    # Write rest of files to file for later
+    with open(PICTURE_LIST_FILE, 'w') as writer:
+        writer.write('\n'.join(i for i in files))
+    return file_name
 
 def main():
-    files = reset_files()
     log = setup_logger()
-    while True:
-        if len(files) == 0:
-            log.warning("No more files left, resetting file list")
-            files = reset_files()
-        file_name = os.path.join(PICTURE_DIRECTORY, files.pop(0))
-        # add condition to check if file doesnt exist
-        if not os.path.isfile(file_name):
-            log.error("Unable to find file:%s, resetting file list", file_name)
-            files = reset_files()
-            continue
-        log.debug("Changing background to file name:%s", file_name)
-        os.system('gsettings set org.gnome.desktop.background picture-uri "file:///%s"' % file_name)
-        os.system('gsettings set org.gnome.desktop.screensaver picture-uri "file:///%s"' % file_name)
-        time.sleep(WAIT_INTERVAL)
+    file_name = reset_files()
+    # add condition to check if file doesnt exist
+    log.debug("Changing background to file name:%s", file_name)
+    os.system('gsettings set org.gnome.desktop.background picture-uri "file:///%s"' % file_name)
+    os.system('gsettings set org.gnome.desktop.screensaver picture-uri "file:///%s"' % file_name)
 
 if __name__ == '__main__':
     main()
