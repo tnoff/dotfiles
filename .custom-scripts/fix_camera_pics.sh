167f531599111ba2b641c9c749d0f2eb1866e41d
tnoff <1326564+tnoff@users.noreply.github.com>
2019-09-17 07:06:07 +0000

    Add mp4s to fix camera pics

--- .custom-scripts/fix_camera_pics.sh
+++ .custom-scripts/fix_camera_pics.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-chmod 664 *.jpg
+chmod 664 *.jpg *.mp4
 
-for pic in *.jpg;
+for pic in *.jpg *.mp4;
 do
     name=$(echo "$pic" | sed -r 's/^([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})(_HDR)?/\1\-\2\-\3\ \4\-\5\-\6/') 
     mv "$pic" "$name"
