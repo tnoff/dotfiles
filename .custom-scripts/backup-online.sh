3087bea8c8f6eba42967254e3cdabafc32a64801
tnoff <1326564+tnoff@users.noreply.github.com>
2020-01-05 19:39:12 -0800

    Update backup paths

--- .custom-scripts/backup-online.sh
+++ .custom-scripts/backup-online.sh
@@ -2,10 +2,10 @@
 
 
 # Run backup actions
-backup-tool directory backup /home/tnorth/Documents/ --overwrite --skip-files ".*/Backup Tool.*" ".*/Secrets.*"
+backup-tool directory backup /home/tnorth/Documents/ --overwrite --skip-files ".*/Secrets.*"
 backup-tool directory backup /home/tnorth/Music/ --overwrite
 backup-tool directory backup /home/tnorth/Pictures/ --overwrite
 backup-tool directory backup /home/tnorth/Videos/ --overwrite
 
 # Copy database over to backup tool
-cp /home/tnorth/.backup-tool/database.sql Documents/Backup\ Tool/$(date +%Y-%m-%d).database.sql
+cp /home/tnorth/.backup-tool/database.sql Documents/Secrets/Backup\ Tool/$(date +%Y-%m-%d).database.sql
